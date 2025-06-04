f9f7fc9f8001b205e3710d51735cac4400a31167
justallyt <albertjunk2030@gmail.com>
2025-06-04 12:54:28 +0300

    Updates

---
+++ client/src/components/common/dashboard/Topbar.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import { PiBellLight } from "react-icons/pi";
+import profileImg from "../../../assets/profile.jpg"
+import { RxChevronDown } from "react-icons/rx";
+
+const Topbar = () => {
+  return (
+    <div className="topbar">
+             <div className="inner-row">
+                       <div className="topbar-content">
+                                 <Link to={"/"}>
+                                           <h1>Dibbly</h1>
+                                  </Link>
+                                  <div className="topbar-column-items">
+                                            <div className="notification-item-wrap">
+                                                      <span className="notify"><PiBellLight /></span>
+                                            </div>
+                                            <div className="profile-item-wrap">
+                                                      <div className="profile-box-item">
+                                                               <div className="profile-image">
+                                                                         <img src={profileImg} alt="" />
+                                                               </div>
+                                                               <h4>Alberto <span><RxChevronDown /></span></h4>
+                                                               
+                                                      </div>
+                                            </div>
+                                  </div>
+                       </div>
+             </div>
+    </div>
+  )
+}
+
+export default Topbar
---
+++ client/src/pages/dashboard/DashboardHome.jsx
@@ -0,0 +1,12 @@
+import Topbar from "../../components/common/dashboard/Topbar"
+import "../../css/dashboard.css"
+
+const DashboardHome = () => {
+  return (
+    <>
+          <Topbar />
+    </>
+  )
+}
+
+export default DashboardHome
